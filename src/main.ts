405d4c0499a6b583867acfd1c5550539a6b0bf66
Hoang Van Thanh <thanh.hoang@finviet.corp>
2020-10-06 13:59:38 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import hbs = require('hbs');
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+
+
+  await app.listen(3000);
+}
+bootstrap();
